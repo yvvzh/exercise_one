bee1cd1395c7ee500602a94940947ef6dd4175f7
yvvzh <135761301+yvvzh@users.noreply.github.com>
2023-07-23 10:03:28 +0200

    added Firebase initial setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'cocktail_screen.dart';
 import 'cocktails.dart';
+import 'firebase_options.dart';
 import 'home_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
